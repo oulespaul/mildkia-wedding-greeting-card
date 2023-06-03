b83447dd17edfca62016929bcb70c3d53ae282ec
Thanakrit choutkhajung <tanakrit.chaukajung@scb.co.th>
2023-06-03 22:51:25 +0700

    feat: init project

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
 
-module.exports = nextConfig
+/** @type {import("next").NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  webpack: (config) => {
+    config.externals = [...config.externals, "canvas", "jsdom"];
+    return config;
+  },
+};
+
+module.exports = withPWA(nextConfig);
